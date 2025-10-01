2a5a53e1363bb96b6c6d09d8d589bb6653fa94af
anantkpatel <imagine231216@gmail.com>
2025-10-02 00:17:52 +0530

    g

--- refokuss/src/Components/Navbar.jsx
+++ refokuss/src/Components/Navbar.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='max-w-screen-xs mx-auto py-6 bg-sky-500 flex items-center w-[70vw] justify-start gap-10 ' >
+    <div className='max-w-screen-xs mx-auto py-6 bg-sky-900 flex items-center w-[70vw] justify-start gap-10 ' >
         <img src="https://i.pinimg.com/736x/92/c9/c4/92c9c49f4e0316573a1c9532eafbc66f.jpg" alt="" className='w-[5vw] h-[10vh] ml-2 ' />
        <div className="links flex gap-14 ml-20">
-         {['Home','About','Contact','Blog','Careers'].map((item)=>(<a href="" className='text-white' >{item} </a>))}
+         {['Home','About','Contact','Blog','Careers'].map((item)=>(<a href="" className='text-white flex items-center g-1' >
+            <span className="inline-block w-1 h-1 rounded-full bg-green-300 "></span>
+            {item} </a>))}
        </div>
     </div>
   )
