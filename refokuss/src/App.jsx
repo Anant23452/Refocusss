e9adcab901da18cf8f7534bd0e663fb5b9f9084c
anantkpatel <imagine231216@gmail.com>
2025-10-01 23:43:04 +0530

    ghl

---
+++ refokuss/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <div className="bg-gray-900  w-[100vw] h-[100vh] ">
+   
+   </div>
+   
+   </>
+  )
+}
+
+export default App
