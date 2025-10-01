f5f40ffb361f492c1612051a9f160fc9d3c2402f
anantkpatel <imagine231216@gmail.com>
2025-10-02 01:09:32 +0530

    hlj

--- refokuss/src/App.jsx
+++ refokuss/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
+import Work from './Components/Work'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <>
    <div className="bg-gray-900  w-screen  h-screen text-white">
    <Navbar/>
+   <Work/>
    </div>
    
    </>
---
+++ refokuss/src/Components/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <div className=' w-full bg-sky-100'>
+        <div className="max-w-screen-xl mx-auto bg-orange-400"></div>
+        </div>
+  )
+}
+
+export default Work
