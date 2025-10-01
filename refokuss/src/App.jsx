907434c6f37c5922b3bc7423fcd31bdef06686b2
anantkpatel <imagine231216@gmail.com>
2025-10-02 00:14:21 +0530

    klk

--- refokuss/src/App.jsx
+++ refokuss/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
    <>
-   <div className="bg-gray-900  w-full  h-screen text-white">
+   <div className="bg-gray-900  w-screen  h-screen text-white">
    <Navbar/>
    </div>
    
--- refokuss/src/Components/Navbar.jsx
+++ refokuss/src/Components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='max-w-screen-xl mx-auto p-3 bg-sky-500' >Navbar</div>
+    <div className='max-w-screen-xs mx-auto py-6 bg-sky-500 flex items-center w-[70vw] justify-start gap-10 ' >
+        <img src="https://i.pinimg.com/736x/92/c9/c4/92c9c49f4e0316573a1c9532eafbc66f.jpg" alt="" className='w-[5vw] h-[10vh] ml-2 ' />
+       <div className="links flex gap-14 ml-20">
+         {['Home','About','Contact','Blog','Careers'].map((item)=>(<a href="" className='text-white' >{item} </a>))}
+       </div>
+    </div>
   )
 }
 
