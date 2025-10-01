fde19a1797f9ed65b64211fbdaae51a269d9cf75
anantkpatel <imagine231216@gmail.com>
2025-10-02 01:07:00 +0530

    s

--- refokuss/src/Components/Navbar.jsx
+++ refokuss/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 function Navbar() {
   return (
-    <div className='max-w-screen-xs mx-auto py-6 bg-gray-800 flex items-center w-[70vw] justify-between gap-1 px-6 ' >
+    <div className='max-w-screen-xs mx-auto py-6 bg-gray-900 flex items-center w-[70vw] justify-between gap-1 px-6 border-b-[1px] border-zinc-500 ' >
        <div className="nleft flex items-center">
          <img src="https://i.pinimg.com/736x/92/c9/c4/92c9c49f4e0316573a1c9532eafbc66f.jpg" alt="" className='w-[2vw] h-[4vh]  ' />
        <div className="links flex gap-10 ml-7">
