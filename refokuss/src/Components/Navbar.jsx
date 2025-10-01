134d897bd00896ffe5a826e8bdfd08e6f38e3204
anantkpatel <imagine231216@gmail.com>
2025-10-02 00:51:18 +0530

    lkll

--- refokuss/src/Components/Navbar.jsx
+++ refokuss/src/Components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='max-w-screen-xs mx-auto py-6 bg-sky-900 flex items-center w-[70vw] justify-start gap-10 ' >
-        <img src="https://i.pinimg.com/736x/92/c9/c4/92c9c49f4e0316573a1c9532eafbc66f.jpg" alt="" className='w-[5vw] h-[10vh] ml-2 ' />
-       <div className="links flex gap-14 ml-20">
-         {['Home','About','Contact','Blog','Careers'].map((item)=>(<a href="" className='text-white flex items-center g-1' >
-            <span className="inline-block w-1 h-1 rounded-full bg-green-300 "></span>
+    <div className='max-w-screen-xs mx-auto py-6 bg-gray-800 flex items-center w-[70vw] justify-start gap-1 px-6 ' >
+        <img src="https://i.pinimg.com/736x/92/c9/c4/92c9c49f4e0316573a1c9532eafbc66f.jpg" alt="" className='w-[2vw] h-[4vh]  ' />
+       <div className="links flex gap-10 ml-7">
+         {['Home','About','Contact','Blog','Career'].map((item,index)=>(<a href="" className='text-white flex items-center g-1' >
+            {item.length===6?<span className='w-[2px] h-3 bg-zinc-500 mr-1 '></span>:null}
+           {index===1 && <span style={{boxShadow:"0 0 0.25em #00FF19"}} className="inline-block w-1 h-1 rounded-full bg-green-300 "></span>}
             {item} </a>))}
        </div>
     </div>
