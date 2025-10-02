b1802a25edacdbda92ae6b287a8da2c7ce569cab
anantkpatel <imagine231216@gmail.com>
2025-10-02 16:10:50 +0530

    lkj

--- refokuss/src/Components/Card.jsx
+++ refokuss/src/Components/Card.jsx
@@ -4,13 +4,16 @@ import { GoArrowRight } from "react-icons/go";
 function Card() {
   return (
     <div className='w-1/2 bg-zinc-700 p-5 rounded-xl' >
-      <div className="">
+      <div className="w-full">
           <div className="w-full flex items-center justify-between">
-            <h1>one heading</h1>
+            <h3>one heading</h3>
             <GoArrowRight  />
 
         </div>
-        <h1>whatever heading</h1>
+        <h1 className='text-3xl semibold'>whatever heading</h1>
+      </div>
+      <div className="down w-full mt-60">
+        <h1 className="text-4xl  font-bold tracking-tight leading-none">Start a project</h1>
       </div>
     </div>
   )
