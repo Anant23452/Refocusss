b984e2ecd6b6e7b34f4646e36c2f84642a5a6b06
anantkpatel <imagine231216@gmail.com>
2025-10-02 11:12:15 +0530

    jlk

--- refokuss/src/Components/Stripe.jsx
+++ refokuss/src/Components/Stripe.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Stripe() {
+function Stripe({val}) {
   return (
-    <div className='min-w-60 x-4 py=3 border-t-2 border-b-2 border-r-2 flex justify-between border-zinc-200  '>
-      <span>image</span>
-      <span className='font-semibold'>34</span>
+    <div className='w-[16.6%] px-2 py=3 border-t-2 border-b-2 border-r-2 flex justify-between border-zinc-600 items-center   '>
+      <img className='w-10 h-12' src={val.url} alt="" />
+      <span className='font-semibold'>{val.number} </span>
     </div>
   )
 }
--- refokuss/src/Components/Stripes.jsx
+++ refokuss/src/Components/Stripes.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Stripe from './Stripe'
 
 function Stripes() {
-  var data =[{url:"",number:"48"},
-{url:"",number:"24"},
-{url:"",number:"12"},
-{url:"",number:"6"},
-{url:"",number:"3"},
-{url:"",number:"2"},
-{url:"",number:"1"}
+  var data =[{url:"https://i.pinimg.com/736x/93/7d/b6/937db623e28b571bc55aa1560cbba1f0.jpg",number:"48"},
+{url:"https://i.pinimg.com/736x/9f/e7/fe/9fe7fe99de3b2afa7b75dd353345f901.jpg",number:"24"},
+{url:"https://i.pinimg.com/736x/09/50/aa/0950aaf7b4e9d8f73768839072744ed8.jpg",number:"12"},
+{url:"https://i.pinimg.com/736x/e5/d7/7c/e5d77c8dd7e42e65820a508d0714c2ff.jpg",number:"6"},
+{url:"https://i.pinimg.com/736x/52/aa/ec/52aaec53ae51a59e3d8df5e496c498de.jpg",number:"3"},
+{url:"https://i.pinimg.com/736x/1f/01/04/1f010495feaea5b6f995fa0947695a3e.jpg",number:"2"},
+
   ]
   return (
-    <div className='flex gap-4 overflow-x-scroll p-4 bg-red-600' >
-   <Stripe/>
+    <div className='flex gap-4 overflow-x-scroll p-4 mt-20  ' >
+  {data.map((item,index)=>{
+    return   <Stripe key={index} val ={item} />
+  })}
     </div>
   )
 }
