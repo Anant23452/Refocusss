db85372c04136e97281da73bac3ff8f878e03503
anantkpatel <imagine231216@gmail.com>
2025-10-02 10:56:48 +0530

    lk

---
+++ refokuss/src/Components/Stripe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Stripe() {
+  return (
+    <div className='min-w-60 x-4 py=3 border-t-2 border-b-2 border-r-2 flex justify-between border-zinc-200  '>
+      <span>image</span>
+      <span className='font-semibold'>34</span>
+    </div>
+  )
+}
+
+export default Stripe
--- refokuss/src/Components/Stripes.jsx
+++ refokuss/src/Components/Stripes.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Stripe from './Stripe'
 
 function Stripes() {
+  var data =[{url:"",number:"48"},
+{url:"",number:"24"},
+{url:"",number:"12"},
+{url:"",number:"6"},
+{url:"",number:"3"},
+{url:"",number:"2"},
+{url:"",number:"1"}
+  ]
   return (
-    <>
-    
-    </>
+    <div className='flex gap-4 overflow-x-scroll p-4 bg-red-600' >
+   <Stripe/>
+    </div>
   )
 }
 
