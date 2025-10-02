eafe415d94f80beb82d0ee6145ae8a2d647459c0
anantkpatel <imagine231216@gmail.com>
2025-10-02 12:32:30 +0530

    lk

--- refokuss/src/Components/Button.jsx
+++ refokuss/src/Components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { SlActionRedo } from "react-icons/sl";
 
-function Button() {
+function Button({title="Get started"}) {
   return (
     <div className='w-fit px-3 py-2 bg-zinc-100 text-black rounded-full'>
-        <span className="text-sm font-medium">Get started</span>
+        <span className="text-sm font-medium">{title}</span>
         <SlActionRedo className='inline-block ml-2 '/>
         </div>
   )
