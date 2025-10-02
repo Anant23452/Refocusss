d2b6681822dcc1cdcd4bb33c9e4f1996c85e7efa
anantkpatel <imagine231216@gmail.com>
2025-10-02 10:08:19 +0530

    kl

--- refokuss/src/App.jsx
+++ refokuss/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Work from './Components/Work'
+import Stripes from './Components/Stripes'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
    <div className="bg-gray-900  w-screen  h-screen text-white">
    <Navbar/>
    <Work/>
+   <Stripes/>
    </div>
    
    </>
