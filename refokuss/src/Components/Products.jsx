f7c0b7041af72aeec15a6d3695ce36d1b2fcd2c7
anantkpatel <imagine231216@gmail.com>
2025-10-02 19:06:08 +0530

    kjo

--- refokuss/src/Components/Products.jsx
+++ refokuss/src/Components/Products.jsx
@@ -8,10 +8,13 @@ function Products() {
     {title:"Abhisekh",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false}
     ]
   return (
-    <div className='bg-gray-900 mt-10' >
+    <div className='bg-gray-900 mt-10 relative' >
         {products.map((item,index)=>{
             return <Product index={index} item={item} />
         })}
+        <div className="w-full h-full  absolute top-0 pointer-events-none  ">
+          <div className="absolute bg-blue-200 w-[32rem] h-[23rem] left-1/3 "></div>
+        </div>
     </div>
   )
 }
