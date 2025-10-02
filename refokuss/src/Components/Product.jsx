b93c66c4814bd3e1a025ff1d13737dc4b9d930cd
anantkpatel <imagine231216@gmail.com>
2025-10-03 00:12:08 +0530

    lkjo

--- refokuss/src/Components/Product.jsx
+++ refokuss/src/Components/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Button from './Button'
 
-function Product({item}) {
+function Product({item,move,count}) {
   return (
     <div className='w-full py-20 px-40 text-white h-[23rem] '>
-        <div className="max-w-screen-xl mx-auto flex items-center justify-between ">
+        <div onMouseEnter={()=>{move(count)}}  className="max-w-screen-xl mx-auto flex items-center justify-between ">
             <h1 className='text-6xl capitalize font-semibold'>{item.title} </h1>
             <div className="dets w-1/3  p-4 rounded-lg">
                 <p>{item.desc} </p>
--- refokuss/src/Components/Products.jsx
+++ refokuss/src/Components/Products.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import Product from './Product'
+import React, { useState } from "react";
+import Product from "./Product";
+import { motion } from "motion/react";
 
 function Products() {
-    var products=[{title:"arquit",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false},
-    {title:"anika",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false},
-    {title:"anant",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:true},
-    {title:"Abhisekh",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false}
-    ]
+  const [pos, setPos] = useState(0);
+  
+  const move =(val)=>{
+    setPos(val*23)
+  }
+  var products = [
+    {
+      title: "arquit",
+      desc: "Lorem ipsum dolor sit amet consectetur eligendi.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "anika",
+      desc: "Lorem ipsum dolor sit amet consectetur eligendi.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "anant",
+      desc: "Lorem ipsum dolor sit amet consectetur eligendi.",
+      live: true,
+      case: true,
+    },
+    {
+      title: "Abhisekh",
+      desc: "Lorem ipsum dolor sit amet consectetur eligendi.",
+      live: true,
+      case: false,
+    },
+  ];
   return (
-    <div className='bg-gray-900 mt-10 relative' >
-        {products.map((item,index)=>{
-            return <Product index={index} item={item} />
-        })}
-        <div className="w-full h-full  absolute top-0 pointer-events-none  ">
-          <div className="absolute bg-blue-200 w-[32rem] h-[23rem] left-1/3 "></div>
-        </div>
+    <div className="bg-gray-900 mt-10 relative">
+      {products.map((item, index) => {
+        return <Product count={index} item={item} move={move} />;
+      })}
+      <div className="w-full h-full  absolute top-0 pointer-events-none  ">
+        <motion.div
+          initial={{ y: pos, x: "-30%" }}
+          animate ={{y:pos+`rem`}}
+          
+          className="window absolute bg-blue-200 w-[25rem] h-[23rem] left-[40%] overflow-hidden "
+        >
+          <motion.div animate={{y:pos*23 +`rem`}} className="seen bg-blue-600 w-full h-full  "></motion.div>
+          <motion.div animate={{y:pos*23 +`rem`}} className="seen bg-blue-600 w-full h-full  "></motion.div>
+          <motion.div animate={{y:pos*23 +`rem`}} className="seen bg-blue-600 w-full h-full  "></motion.div>
+          <motion.div animate={{y:pos*23 +`rem`}} className="seen bg-blue-600 w-full h-full  "></motion.div>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
