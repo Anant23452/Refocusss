7d1051b2845005dd807096e6bd62fc9a089b6891
anantkpatel <imagine231216@gmail.com>
2025-10-02 17:55:33 +0530

    fh

--- refokuss/src/Components/Work.jsx
+++ refokuss/src/Components/Work.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion, useScroll,useMotionValueEvent } from "motion/react"
+import { motion, useScroll,useMotionValueEvent} from "motion/react"
 
 const images=[
     {url:"https://cdn.prod.website-files.com/664dc8b6bc52b504509197e4/6867e5af4ce59d080b9ebf42_branding-big.webp",top:"50%",left:"50%",isActive:true},
@@ -9,7 +9,7 @@ const images=[
     {url:"https://i.pinimg.com/1200x/eb/d2/c9/ebd2c947fa4e41abe889f8646be506bc.jpg",top:"43%",left:"40%",isActive:false},
     {url:"https://i.pinimg.com/736x/1d/e5/29/1de52947cf871b610fb4276053dd137f.jpg",top:"65%",left:"55%",isActive:false},
 ]
-const{scrollY}= useScroll();
+const {scrollY}= useScroll();
 useMotionValueEvent(scrollY, "change", (latest) => {
   console.log("Page scroll: ", latest)
 })
