198ac5cf829239134a8b4f2039c12baba1c9fc98
anantkpatel <imagine231216@gmail.com>
2025-10-02 16:00:30 +0530

    jh

---
+++ refokuss/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-1/2 bg-zinc-700 p-5 rounded-xl' >
+
+    </div>
+  )
+}
+
+export default Card
---
+++ refokuss/src/Components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+function Cards() {
+  return (
+    <div className='w-full bg-red-700 '>
+        <div className="w-[70vw] max-w-screen-xl mx-auto bg-green-700 py-20 flex gap-5">
+
+        <Card/>
+        <Card/>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
