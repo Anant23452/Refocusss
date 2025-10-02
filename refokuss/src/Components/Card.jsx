5c5cf0de7bc618b03e56366c660c246ee34bec17
anantkpatel <imagine231216@gmail.com>
2025-10-02 16:24:59 +0530

    lkoi

--- refokuss/src/Components/Card.jsx
+++ refokuss/src/Components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { GoArrowRight } from "react-icons/go";
 
-function Card() {
+function Card({width,start,para}) {
   return (
-    <div className='w-1/2 bg-zinc-700 p-5 rounded-xl' >
+    <div className={`bg-zinc-700 p-5 rounded-xl ${width}`} >
       <div className="w-full">
           <div className="w-full flex items-center justify-between">
             <h3>one heading</h3>
@@ -13,8 +13,14 @@ function Card() {
         <h1 className='text-3xl semibold'>whatever heading</h1>
       </div>
       <div className="down w-full mt-60 ">
-        <h1 className="text-4xl  font-bold tracking-tight leading-none">Start a project</h1>
+       {
+        start===true &&(
+            <>
+             <h1 className="text-4xl  font-bold tracking-tight leading-none">Start a project</h1>
         <button className="rounded-full py-2 px-3 mt-5 border-1 border-zinc-50">Contact Us</button>
+            </>
+        )
+       }
         <p className='text-sm text-zinc-5-- font-medium mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
       </div>
     </div>
--- refokuss/src/Components/Cards.jsx
+++ refokuss/src/Components/Cards.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 function Cards() {
   return (
     <div className='w-full bg-red-700 '>
-        <div className="w-[80vw] max-w-screen-xl mx-auto bg-green-700 py-20 flex gap-5">
+        <div className="w-[80vw] max-w-screen-xl mx-auto bg-green-700 py-20 flex gap-2">
 
-        <Card/>
-        <Card/>
+        <Card width={"basis-1/3"} start={false} para={true} />
+        <Card width={"basis-2/3"} start={true} para ={false} />
         </div>
     </div>
   )
