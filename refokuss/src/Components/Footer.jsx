3cc1512e0838deeaa29fd02266ebbcaeee9c6935
anantkpatel <imagine231216@gmail.com>
2025-10-02 16:50:38 +0530

    lk

--- refokuss/src/Components/Footer.jsx
+++ refokuss/src/Components/Footer.jsx
@@ -7,7 +7,14 @@ function Footer() {
             <div className="basis-1/2 bg-red-600">
             <h1 className='text-[11rem] font-semibold tracking-tigh leading-none '>refocuss.</h1>
             </div>
-            <div className="basis-1/2 bg-green-600">hello</div>
+            <div className="basis-1/2 bg-green-600">
+            <div className="">
+                <h4>socials</h4>
+              {
+                 ["social","twitter","linkedIn"].map((item,index)=><a className='block mt-2 capitalize text-zinc-500' href="" >{item}</a> )
+              } 
+            </div>
+            </div>
         </div>
 
     </div>
