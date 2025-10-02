f75e4e1b689e25cc7a1bf84571030d3393e896a5
anantkpatel <imagine231216@gmail.com>
2025-10-02 12:27:45 +0530

    lk

--- refokuss/src/Components/Products.jsx
+++ refokuss/src/Components/Products.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Product from './Product'
 
 function Products() {
+    var products=[{title:"arquit",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false},
+    {title:"anika",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false},
+    {title:"anant",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false},
+    {title:"Abhisekh",desc:"Lorem ipsum dolor sit amet consectetur eligendi.",live:true,case:false}
+    ]
   return (
     <div>
         <Product/>
