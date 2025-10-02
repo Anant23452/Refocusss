6c6d1230fbbdf4434c1d7ab0dfd5b9762eaa0cb0
anantkpatel <imagine231216@gmail.com>
2025-10-02 12:38:28 +0530

    k;l

--- refokuss/src/Components/Product.jsx
+++ refokuss/src/Components/Product.jsx
@@ -6,10 +6,12 @@ function Product({item}) {
     <div className='w-full py-20 px-40 text-white'>
         <div className="max-w-screen-xl mx-auto flex items-center justify-between ">
             <h1 className='text-6xl capitalize font-semibold'>{item.title} </h1>
-            <div className="dets w-1/4 bg-gray-800 p-4 rounded-lg">
+            <div className="dets w-1/3 bg-gray-800 p-4 rounded-lg">
                 <p>{item.desc} </p>
-               {item.live && <Button  />}
+              <div className="flex items-center gap-5">
+                 {item.live && <Button  />}
                {item.case && <Button title='case study'  />}
+              </div>
             </div>
         </div>
     </div>
